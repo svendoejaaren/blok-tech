6b08b45041ce5cb435e549211b72dc7e791a671f
svendoejaaren <svendoejaaren@hotmail.com>
2022-02-15 15:11:12 +0100

    updates

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
+app.use('/static', express.static('static'))
+  
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.status(200).send('Hello World!')
+})
+  
+app.get('/results', (req, res) => {
+    res.status(200).send('<h1>Resultaten</h1>')
+})
+  
+app.get('/song', (req, res) => {
+    res.status(200).send('<h1>Layla</h1>')
+})
+  
+app.get('*', (req, res) => {
+    res.status(404).send('Deze pagina bestaat niet... Jammer')
 })
 
 app.listen(port, () => {
